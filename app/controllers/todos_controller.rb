7c797076e853547957152c87514ad461b3f5314c
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2023-01-06 15:07:49 +0530

    CRUD functionality added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,4 +10,29 @@ class TodosController < ApplicationController
     def new
         @todo = Todo.new
     end
+
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(todo_params)
+        redirect_to todos_path
+    end
+
+    def create
+        todo = Todo.create(todo_params)
+        redirect_to todos_path
+    end
+
+    def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+    end
+
+    private def todo_params
+        params.require(:todo).permit(:title,:completed)
+    end
 end
